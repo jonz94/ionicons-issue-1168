899fbf7e30b1f32eef33d17080e0422435f9ade2
jonz94 <jody16888@gmail.com>
2023-01-14 12:44:41 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'i6-ng15-play',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
